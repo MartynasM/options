c04cd4d4dfb6715e10b07bef2dff22c97c5df5f2
Martynas <martynas@samesystem.dk>
2011-11-14 11:46:15 +0200

    Change class name

--- git/hooks/pre-commit-hooks/hook.rb
+++ git/hooks/pre-commit-hooks/hook.rb
@@ -1,5 +1,5 @@
 class Hook
-  require File.expand_path(File.join(File.dirname(__FILE__),  "git_world"))
+  require File.expand_path(File.join(File.dirname(__FILE__),  "git_result"))
   require 'open3'
   include Open3
 
@@ -23,12 +23,12 @@ class Hook
   # @compiler_ruby = `which ruby`.strip if compiler_ruby.length == 0 
     @compiler_ruby = `which ruby`.strip
 
-    @world = GitWorld.new(stop_on_warnings)
+    @result = GitResult.new(stop_on_warnings)
     @changed_ruby_files = `git diff-index --name-only --cached HEAD`.split("\n").select{ |file| file =~ @files_to_watch }.map(&:chomp)
   end
 
   def each_changed_file
-    if @world.continue?
+    if @result.continue?
       @changed_ruby_files.each do |file|
         yield file if File.readable?(file)
       end
@@ -40,7 +40,7 @@ class Hook
       if file =~ @rb_regexp
         popen3("#{@compiler_ruby} -wc #{file}") do |stdin, stdout, stderr|
           stderr.read.split("\n").each do |line|
-            line =~ @rb_warning_regexp ? @world.warnings << line : @world.errors << line 
+            line =~ @rb_warning_regexp ? @result.warnings << line : @result.errors << line 
           end
         end
         end
@@ -51,7 +51,7 @@ class Hook
     each_changed_file do |file|
       if file =~ @erb_regexp
         popen3("rails-erb-check #{file}") do |stdin, stdout, stderr|
-          @world.errors.concat stdout.read.split("\n").map{|line| "#{file} => invalid ERB syntax" if line.gsub(@color_regexp, '') =~ @erb_invalid_regexp}.compact
+          @result.errors.concat stdout.read.split("\n").map{|line| "#{file} => invalid ERB syntax" if line.gsub(@color_regexp, '') =~ @erb_invalid_regexp}.compact
         end
       end
     end
@@ -61,19 +61,19 @@ class Hook
     each_changed_file do |file|
       if file =~ @rb_regexp or file =~ @erb_regexp
         popen3("rails_best_practices #{file}") do |stdin, stdout, stderr|
-          @world.warnings.concat stdout.read.split("\n")
+          @result.warnings.concat stdout.read.split("\n")
         end
       end
     end
   end
 
   # Maybe need options for different file types :rb :erb :js
-  def warnning_on(string)
+  def warning_on(string)
     each_changed_file do |file|
       popen3("fgrep #{string} #{file}") do |stdin, stdout, stderr|
         err = stdout.read
         if err.split("\n").size > 0 
-          @world.warnings << "#{string} in #{file}:"
+          @result.warnings << "#{string} in #{file}:"
         end
       end
     end
@@ -81,17 +81,17 @@ class Hook
 
   def results
     status = 0
-    if @world.errors.size > 0 
+    if @result.errors.size > 0 
       status = 1
       puts "ERRORS:\n"
-      puts @world.errors.join("\n\n")
+      puts @result.errors.join("\n\n")
     end
 
-    if @world.warnings.size > 0 
+    if @result.warnings.size > 0 
       status = 1 if @stop_on_warnings
 
       puts "Warnings: \n"
-      puts @world.warnings.join("\n\n")
+      puts @result.warnings.join("\n\n")
     end
     return status
   end
